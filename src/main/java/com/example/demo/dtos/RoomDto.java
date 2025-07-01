aad399fb1ad9c000c7efc519b530e6787557fdf5
diegooo555 <dieguito555alf@outlook.es>
2025-06-30 21:18:18 -0500

    New Property in Room Dto.

--- src/main/java/com/example/demo/dtos/RoomDto.java
+++ src/main/java/com/example/demo/dtos/RoomDto.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,5 +14,6 @@ public class RoomDto {
     private String description;
     private BigDecimal price;
     private String urlImg;
+    private List<String> images;
     private Integer capacity;
 }
